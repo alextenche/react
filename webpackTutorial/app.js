28b1a0a639e88c86ddbfe7701c3a64894a5ceb81
Alexandru Tenche <alex.tenche@gmail.com>
2016-10-03 17:57:21 +0300

    added webpackTutorial

---
+++ webpackTutorial/app.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Scoreboard from './components/Scoreboard';
+
+import './css/style.css';
+
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
